a030eb26d1d035fa403427f0df39c03ca89229ab
Diego Imbus <diego.imbus@globant.com>
2020-06-29 13:19:23 -0500

    adding branches and info on carrusel

--- packages/shop/data/multi-carousel-demo-data.ts
+++ packages/shop/data/multi-carousel-demo-data.ts
@@ -1,6 +1,14 @@
 import OfferOne from '../image/offer-1.png';
 import OfferThree from '../image/offer-3.png';
 import OfferTwo from '../image/offer-2.png';
+import OfferFour from '../image/offer-4.png';
+import OfferFive from '../image/offer-5.png';
+import OfferSix from '../image/offer-6.png';
+import OfferSeven from '../image/offer-7.png';
+import OfferEight from '../image/offer-8.png';
+import OfferNine from '../image/offer-9.png';
+import OfferTen from '../image/offer-10.png';
+
 
 export default [
   {
@@ -17,5 +25,40 @@ export default [
     id: '3',
     url: OfferThree,
     alt: 'Offer 3',
+  },
+  {
+    id: '4',
+    url: OfferFour,
+    alt: 'Offer 4',
+  },
+  {
+    id: '5',
+    url: OfferFive,
+    alt: 'Offer 5',
+  },
+  {
+    id: '6',
+    url: OfferSix,
+    alt: 'Offer 6',
+  },
+  {
+    id: '7',
+    url: OfferSeven,
+    alt: 'Offer 7',
+  },
+  {
+    id: '8',
+    url: OfferEight,
+    alt: 'Offer 8',
+  },
+  {
+    id: '9',
+    url: OfferNine,
+    alt: 'Offer 9',
+  },
+  {
+    id: '10',
+    url: OfferTen,
+    alt: 'Offer 10',
   },
 ];
--- packages/shop/data/offers.ts
+++ packages/shop/data/offers.ts
@@ -1,6 +1,13 @@
 import OfferOne from '../image/offer-1.png';
 import OfferThree from '../image/offer-3.png';
 import OfferTwo from '../image/offer-2.png';
+import OfferFour from '../image/offer-4.png';
+import OfferFive from '../image/offer-5.png';
+import OfferSix from '../image/offer-6.png';
+import OfferSeven from '../image/offer-7.png';
+import OfferEight from '../image/offer-8.png';
+import OfferNine from '../image/offer-9.png';
+import OfferTen from '../image/offer-10.png';
 
 export default [
   {
@@ -17,5 +24,40 @@ export default [
     id: '3',
     imgSrc: OfferThree,
     alt: 'Offer 3',
+  },
+  {
+    id: '4',
+    imgSrc: OfferFour,
+    alt: 'Offer 4',
+  },
+  {
+    id: '5',
+    imgSrc: OfferFive,
+    alt: 'Offer 5',
+  },
+  {
+    id: '6',
+    imgSrc: OfferSix,
+    alt: 'Offer 6',
+  },
+  {
+    id: '7',
+    imgSrc: OfferSeven,
+    alt: 'Offer 7',
+  },
+  {
+    id: '8',
+    imgSrc: OfferEight,
+    alt: 'Offer 8',
+  },
+  {
+    id: '9',
+    imgSrc: OfferNine,
+    alt: 'Offer 9',
+  },
+  {
+    id: '10',
+    imgSrc: OfferTen,
+    alt: 'Offer 10',
   },
 ];
